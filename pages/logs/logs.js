317cfe454f87ed0e0a768e36963d3863cc058fa0
small_winder <longtengfly@163.com>
2018-02-07 10:54:15 +0800

    Add files via upload

---
+++ pages/logs/logs.js
@@ -0,0 +1,21 @@
+var util = require( '../../utils/util.js' )
+Page( {
+  data: {
+    projects: [ 
+    ]
+  },
+  onReady: function() {
+    this.clickName();
+  },
+  clickName: function( e ) {
+    var pros = this.data.projects;
+    console.log( "#########################################################################################################" )
+    console.log( "##                                               其他项目                                               ##" )
+    console.log( "##-----------------------------------------------------------------------------------------------------##" )
+    pros.forEach( function( item, index ) {
+      console.log( "##        ", item.name + ":" + item.git )
+    })
+    console.log( "##                                                                                                     ##" )
+    console.log( "#########################################################################################################" )
+  }
+})
